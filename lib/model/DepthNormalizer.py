c80b153f326172687fd60daedcf5ed9fea531be6
ashok-vardan <140464241+ashok-vardan@users.noreply.github.com>
2023-07-26 22:33:32 +0530

    Update DepthNormalizer.py

--- lib/model/DepthNormalizer.py
+++ lib/model/DepthNormalizer.py
@@ -1,5 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F 
@@ -15,6 +13,32 @@ class DepthNormalizer(nn.Module):
         args:
             xyz: [B, 3, N] depth value
         '''
-        z_feat = xyz[:,2:3,:] * (self.opt.loadSize // 2) / self.opt.z_size
-
+        z_feat = xyz[:, 2:3, :] * (self.opt.loadSize // 2) / self.opt.z_size
         return z_feat
+
+class Generator(nn.Module):
+    def __init__(self, opt):
+        super(Generator, self).__init__()
+        self.opt = opt
+        # Other layers of your generator can be defined here...
+
+        # Add the DepthNormalizer module
+        self.depth_normalizer = DepthNormalizer(opt)
+
+    def forward(self, input):
+        # Your generator forward pass logic goes here...
+        # For example:
+        # x = self.conv1(input)
+        # x = self.conv2(x)
+        # ...
+
+        # Apply the DepthNormalizer to the output of your generator
+        normalized_depth = self.depth_normalizer(x)
+        return x, normalized_depth
+
+# Example usage:
+opt = {'loadSize': 256, 'z_size': 100}  # Replace with appropriate values
+generator = Generator(opt)
+input_xyz = torch.randn(2, 3, 100)  # Example input depth values (batch size 2, 100 points)
+output, normalized_depth = generator(input_xyz)
+print(normalized_depth)
