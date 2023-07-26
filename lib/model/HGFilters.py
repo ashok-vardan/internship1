fb3e14fd4c4544a478ee740b2fcf52b25ae4b46f
ashok-vardan <140464241+ashok-vardan@users.noreply.github.com>
2023-07-26 22:53:55 +0530

    Update HGFilters.py

--- lib/model/HGFilters.py
+++ lib/model/HGFilters.py
@@ -27,3 +27,36 @@ class Generator(nn.Module):
         outputs, _ = self.hg_filter(x)
         generated_output = self.generator_layers(outputs[-1])  # Use the output of the last stack
         return generated_output
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, num_filters=64):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, num_filters, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(num_filters, num_filters * 2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_filters * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(num_filters * 2, num_filters * 4, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_filters * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(num_filters * 4, num_filters * 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_filters * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(num_filters * 8, 1, kernel_size=4, stride=1, padding=0),
+            nn.Sigmoid()  # Use Sigmoid activation for binary classification
+        )
+
+    def forward(self, x):
+        return self.model(x)
+class GAN(nn.Module):
+    def __init__(self, stack, depth, in_ch, last_ch, norm='batch', down_type='conv64', use_sigmoid=True):
+        super(GAN, self).__init__()
+        self.generator = Generator(stack, depth, in_ch, last_ch, norm, down_type, use_sigmoid)
+        self.discriminator = Discriminator(last_ch)
+
+    def forward(self, x):
+        generated_output = self.generator(x)
+        discriminator_output = self.discriminator(generated_output)
+        return generated_output, discriminator_output
+
